b346cdac4700e5d851a964896a7bb8b0c8ce74c9
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2024-12-21 22:11:21 +0330

    feat: add marketing layout with header and footer components, and update default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <WebVitals />
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
