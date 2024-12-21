68f4769a98a851cad5110715ec1a4b8512884691
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2024-12-21 19:50:54 +0330

    feat: update button styles and variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,26 +5,37 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap uppercase tracking-wide rounded-xl text-sm font-bold transition-colors focus-visible:Ghost-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   {
     variants: {
       variant: {
         default:
-          'bg-primary text-primary-foreground shadow hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
-        outline:
-          'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
+          'bg-white text-black border-slate-200 border-2 border-b-4 active:border-b-2 hover:bg-slate-100 text-slate-500',
+        ghost:
+          'bg-transparent text-slate-500 border-transparent border-0 hover:bg-slate-100',
+        primary:
+          'bg-sky-400 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0',
+        primaryGhost: 'bg-white text-sky-500 hover:bg-slate-100',
         secondary:
-          'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
+          'bg-green-500 text-primary-foreground hover:bg-green-500/90 border-green-600 border-b-4 active:border-b-0',
+        secondaryGhost: 'bg-white text-green-500 hover:bg-slate-100',
+        danger:
+          'bg-rose-500 text-primary-foreground hover:bg-rose-500/90 border-rose-600 border-b-4 active:border-b-0',
+        dangerGhost: 'bg-white text-rose-500 hover:bg-slate-100',
+        super:
+          'bg-indigo-500 text-primary-foreground hover:bg-indigo-500/90 border-indigo-600 border-b-4 active:border-b-0',
+        superGhost: 'bg-white text-indigo-500 hover:bg-slate-100',
+        sidebar:
+          'bg-transparent text-slate-500 border-transparent border-2 hover:bg-slate-100 transition-none',
+        sidebarGhost:
+          'bg-sky-500/15 text-sky-500 border-sky-300 border-2 hover:bg-sky-500/20 transition-none',
       },
       size: {
-        default: 'h-9 px-4 py-2',
-        sm: 'h-8 rounded-md px-3 text-xs',
-        lg: 'h-10 rounded-md px-8',
+        default: 'h-11 px-4 py-2',
+        sm: 'h-8 px-3 text-xs',
+        lg: 'h-12 px-8',
         icon: 'h-9 w-9',
+        rounded: 'rounded-full',
       },
     },
     defaultVariants: {
