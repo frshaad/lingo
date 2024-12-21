e8ca556ae313e2be241f1b51dbfdb805b2e94c9f
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2024-12-21 22:27:05 +0330

    feat: enhance Clerk middleware to protect non-public routes and add required environment variables for sign-in and sign-up URLs

--- src/lib/validated-env.ts
+++ src/lib/validated-env.ts
@@ -7,6 +7,12 @@ const envSchema = z.object({
   CLERK_SECRET_KEY: z.string({
     required_error: 'Clerk secret key is required',
   }),
+  NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string({
+    required_error: 'Clerk sign in url is required',
+  }),
+  NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string({
+    required_error: 'Clerk sign up url is required',
+  }),
 });
 
 const env = envSchema.parse(process.env);
