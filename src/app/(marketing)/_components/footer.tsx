ae31177747ab34c6fe9bb948e22b5ef14f1e440a
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2024-12-22 16:10:00 +0330

    feat: enhance marketing footer with country flags and update layout in marketing page

--- src/app/(marketing)/_components/footer.tsx
+++ src/app/(marketing)/_components/footer.tsx
@@ -1,7 +1,38 @@
+import Image from 'next/image';
+
+import { Button } from '@/components/ui/button';
+
 export default function MarketingFooter() {
   return (
     <footer className="hidden h-20 w-full border-t-2 border-slate-200 p-2 lg:block">
-      Footer
+      <div className="mx-auto flex h-full max-w-screen-lg items-center justify-evenly">
+        {COUNTRIES.map((country) => (
+          <CountryFlag key={country.id} {...country} />
+        ))}
+      </div>
     </footer>
   );
 }
+
+function CountryFlag({ lang, src }: { lang: string; src: string }) {
+  return (
+    <Button size="lg" variant="ghost" className="w-full">
+      <Image
+        src={src}
+        alt={lang}
+        height={32}
+        width={40}
+        className="mr-4 rounded-md capitalize"
+      />
+      {lang}
+    </Button>
+  );
+}
+
+const COUNTRIES = [
+  { id: 1, src: '/hr.svg', lang: 'croatian' },
+  { id: 2, src: '/es.svg', lang: 'spanish' },
+  { id: 3, src: '/fr.svg', lang: 'french' },
+  { id: 4, src: '/it.svg', lang: 'italian' },
+  { id: 5, src: '/jp.svg', lang: 'japanese' },
+];
