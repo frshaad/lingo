6a9638bab2f57c67ee97b6f6e80e114f7ed7b404
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2024-12-23 16:34:29 +0330

    feat: add user progress table and relations to courses schema for tracking user activity

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,34 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+import { integer, pgTable, serial, text } from 'drizzle-orm/pg-core';
 
 export const coursesTable = pgTable('courses', {
   id: serial('id').primaryKey(),
   title: text('title').notNull(),
   imageSrc: text('image_src').notNull(),
 });
+
+export const coursesRelations = relations(coursesTable, ({ many }) => ({
+  userProgress: many(userProgressTable),
+}));
+
+export const userProgressTable = pgTable('user_progress', {
+  userId: text('user_id').primaryKey(),
+  userName: text('user_name').notNull().default('user'),
+  userImageSrc: text('user_image_src').notNull().default('/macot.svg'),
+  activeCourseId: integer('active_course_id').references(
+    () => coursesTable.id,
+    { onDelete: 'cascade' },
+  ),
+  hearts: integer('hearts').notNull().default(5),
+  points: integer('points').notNull().default(0),
+});
+
+export const userProgressRelations = relations(
+  userProgressTable,
+  ({ one }) => ({
+    activeCourses: one(coursesTable, {
+      fields: [userProgressTable.activeCourseId],
+      references: [coursesTable.id],
+    }),
+  }),
+);
