2e2c75f31928a24d34fbeb1fd9f0df14b84058db
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2024-12-24 19:32:31 +0330

    feat: add database seeding functionality and dotenv configuration

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
@@ -8,3 +10,26 @@ import * as schema from './schema';
 const sql = neon(env.DATABASE_URL);
 const db = drizzle(sql, { schema });
 export default db;
+
+async function seed() {
+  try {
+    console.log('Seeding Database...');
+
+    await db.delete(schema.coursesTable);
+    await db.delete(schema.userProgressTable);
+
+    await db.insert(schema.coursesTable).values([
+      { title: 'spanish', imageSrc: '/es.svg' },
+      { title: 'german', imageSrc: '/de.svg' },
+      { title: 'french', imageSrc: '/fr.svg' },
+      { title: 'italian', imageSrc: '/it.svg' },
+    ]);
+
+    console.log('Seeding Finished!');
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to seed the database');
+  }
+}
+
+seed();
