d02c67db17a37f10034d1ffa7614cbdccd54bed2
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2024-12-27 15:25:16 +0330

    feat: add unit retrieval logic and integrate with user progress on LearnPage

--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -1,14 +1,17 @@
 import { redirect } from 'next/navigation';
 
 import UserProgress from '@/components/user-progress';
-import { getUserProgress } from '@/db/queries';
+import { getUnits, getUserProgress } from '@/db/queries';
 
 import FeedHeader from './_components/feed-header';
 import FeedWrapper from './_components/feed-wrapper';
 import StickyWrapper from './_components/sticky-wrapper';
 
 export default async function LearnPage() {
-  const userProgress = await getUserProgress();
+  const [userProgress, units] = await Promise.all([
+    getUserProgress(),
+    getUnits(),
+  ]);
 
   if (!userProgress || !userProgress.activeCourse) {
     redirect('/courses');
@@ -18,6 +21,11 @@ export default async function LearnPage() {
     <div className="flex gap-12 px-6">
       <FeedWrapper>
         <FeedHeader title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            {JSON.stringify(unit)}
+          </div>
+        ))}
       </FeedWrapper>
       <StickyWrapper>
         <UserProgress
--- src/db/queries.ts
+++ src/db/queries.ts
@@ -3,11 +3,20 @@ import { eq } from 'drizzle-orm';
 import { cache } from 'react';
 
 import db from '@/db';
-import { course, userProgress } from '@/db/schema';
+import { course, unit, userProgress } from '@/db/schema';
 
 export const getCourses = cache(async () => {
   const courses = await db.query.course.findMany();
   return courses;
+});
+
+export const getCourseById = cache(async (courseId: number) => {
+  const courseData = await db.query.course.findFirst({
+    where: eq(course.id, courseId),
+    // TODO: Populate units and lessons
+  });
+
+  return courseData;
 });
 
 export const getUserProgress = cache(async () => {
@@ -24,11 +33,43 @@ export const getUserProgress = cache(async () => {
   return userProgressData;
 });
 
-export const getCourseById = cache(async (courseId: number) => {
-  const courseData = await db.query.course.findFirst({
-    where: eq(course.id, courseId),
-    // TODO: Populate units and lessons
+export const getUnits = cache(async () => {
+  const userProgress = await getUserProgress();
+  if (!userProgress?.activeCourseId) {
+    return [];
+  }
+
+  const unitsData = await db.query.unit.findMany({
+    where: eq(unit.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        with: {
+          challenges: {
+            with: {
+              challengeProgresses: true,
+            },
+          },
+        },
+      },
+    },
   });
 
-  return courseData;
+  const normalizedUnitsData = unitsData.map((unit) => {
+    const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      const areAllChallengesCompleted = lesson.challenges.every(
+        (challenge) =>
+          challenge.challengeProgresses &&
+          challenge.challengeProgresses.length > 0 &&
+          challenge.challengeProgresses.every(
+            (progress) => progress.isCompleted,
+          ),
+      );
+
+      return { ...lesson, isCompleted: areAllChallengesCompleted };
+    });
+
+    return { ...unit, lessons: lessonsWithCompletedStatus };
+  });
+
+  return normalizedUnitsData;
 });
