172fd42bddf82652305d8984397053d783c2bc74
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-01-19 14:25:45 +0330

    chore: migrate ESLint configuration to new flat format and update dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import drizzle from 'eslint-plugin-drizzle';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+const eslintConfig = [
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:drizzle/recommended',
+    'prettier',
+  ),
+  {
+    plugins: {
+      drizzle,
+      'simple-import-sort': simpleImportSort,
+    },
+
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'drizzle/enforce-delete-with-where': 'off',
+      'drizzle/enforce-update-with-where': 'off',
+    },
+  },
+];
+
+export default eslintConfig;
