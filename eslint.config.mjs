bb78022485ed7cd5e6282041380c6afc6eb1a99f
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-02-02 14:30:58 +0330

    🔧chore: add TypeScript type definition for ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,7 @@ const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 });
 
+/** @type {import('eslint').Linter.Config[]} */
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
