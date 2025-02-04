52774efb095a831f7d701dc0cd0ee7c7e13eb42c
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-02-04 23:11:05 +0330

    ♻️Refactor: rename ChallengeFinishScreen to LessonFinishedScreen for improved clarity and consistency

--- src/app/lesson/_components/lesson-finished-screen.tsx
+++ src/app/lesson/_components/lesson-finished-screen.tsx
@@ -2,14 +2,14 @@
 
 import { useQuizContext } from '../_context/quiz-context';
 
-export default function ChallengeFinishScreen() {
+export default function LessonFinishedScreen() {
   const { finishAudio } = useQuizContext();
 
   return (
     <>
       {finishAudio}
       <h1 className="text-center text-lg font-bold text-neutral-700 lg:text-start lg:text-3xl">
-        Finished the challenge!
+        Finished the Lesson!
       </h1>
     </>
   );
--- src/app/lesson/_components/quiz.tsx
+++ src/app/lesson/_components/quiz.tsx
@@ -1,15 +1,15 @@
 'use client';
 
 import { useQuizContext } from '../_context/quiz-context';
-import ChallengeFinishScreen from './challenge-finish-screen';
+import ChallengeFinishScreen from './lesson-finished-screen';
 import QuizContent from './quiz-content';
 import QuizFooter from './quiz-footer';
 import QuizHeader from './quiz-header';
 
 export default function Quiz() {
-  const { activeChallenge } = useQuizContext();
+  const { activeChallenge, lessonId } = useQuizContext();
 
-  if (activeChallenge) {
+  if (activeChallenge.lessonId === lessonId) {
     return (
       <>
         <QuizHeader />
