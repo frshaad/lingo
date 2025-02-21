e3c447d3d2a1870570b583ca52968556fc42cc8f
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-02-21 12:03:30 +0330

    ✨ add AuthButtons component for user authentication in landing page

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,43 +1,6 @@
 import Image from 'next/image';
-import Link from 'next/link';
 
-import {
-  ClerkLoaded,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-} from '@clerk/nextjs';
-
-import ClerkLoadingSpinner from '@/components/clerk/clerk-loading';
-import { Button } from '@/components/ui/button';
-
-function AuthButtons() {
-  return (
-    <div className="flex w-full max-w-[330px] flex-col items-center gap-y-3">
-      <ClerkLoadingSpinner />
-      <ClerkLoaded>
-        <SignedOut>
-          <SignUpButton mode="modal">
-            <Button className="w-full" size="lg" variant="secondary">
-              Get Started
-            </Button>
-          </SignUpButton>
-          <SignInButton mode="modal">
-            <Button className="w-full" size="lg" variant="primaryGhost">
-              I already have an account
-            </Button>
-          </SignInButton>
-        </SignedOut>
-        <SignedIn>
-          <Button className="w-full" size="lg" variant="secondary" asChild>
-            <Link href="/learn">Continue Learning</Link>
-          </Button>
-        </SignedIn>
-      </ClerkLoaded>
-    </div>
-  );
-}
+import AuthButtons from './_components/auth-buttons';
 
 export default function Home() {
   return (
